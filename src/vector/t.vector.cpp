2d5f235cdb37952e9044ddcac079048cf99f385e
Guilherme Dantas <guidanoli@hotmail.com>
2020-04-01 17:44:21 -0300

    vector operators, methods for std::move and range based loops + tests

--- src/vector/t.vector.cpp
+++ src/vector/t.vector.cpp
@@ -1,14 +1,71 @@
 #include "vector.hpp"
 
-#include <assert.h>
+#include <cassert>
+
+#define eachin(i, v) (std::size_t i = 0; i < v.size(); ++i)
+
+void test_empty_vector()
+{
+	Vector<int> v;
+	assert(v.size() == 0);
+}
+
+void test_equal_operator_copies_size()
+{
+	Vector<int> v, w(10);
+	
+	assert(v.size() == 0);
+	assert(w.size() == 10);
+	
+	v = w;
+	
+	assert(v.size() == 10);
+	assert(w.size() == 10);
+}
+
+void test_equal_operator_copies_data()
+{
+	Vector<std::size_t> v, w(10);
+
+	for eachin(i, w)
+		w[i] = i;
+
+	v = w;
+
+	for eachin(i, v)
+		assert(v[i] == i);
+
+	for eachin(i, w)
+		w[i] = i + 1;
+
+	for eachin(i, v) {
+		assert(v[i] == i);
+		assert(w[i] == i + 1);
+	}
+}
+
+void test_range_based_loop()
+{
+	Vector<std::size_t> v(10);
+
+	for eachin(i, v)
+		v[i] = i + 137;
+
+	std::size_t iterated = 0;
+	
+	for (auto item : v) {
+		assert(item == v[iterated]);
+		iterated++;
+	}
+	
+	assert(iterated == v.size());
+}
 
 int main(int argc, char **argv)
 {
-	Vector<std::size_t> v(10);
-	assert(v.size() == 10);
-	for (std::size_t i = 0; i < v.size(); ++i) {
-		v.set(i, i*i);
-		assert(v.get(i) == i*i);
-	}
+	test_empty_vector();
+	test_equal_operator_copies_size();
+	test_equal_operator_copies_data();
+	test_range_based_loop();
 	return 0;
 }
--- src/vector/vector.hpp
+++ src/vector/vector.hpp
@@ -8,34 +8,62 @@
 template<typename T>
 class Vector
 {
+public:
+	class iterator
+	{
+	public:
+		iterator(T *ptr) : ptr(ptr) {}
+		iterator& operator++() { ++ptr; return *this; }
+		bool operator!=(const iterator &it) const { return ptr != it.ptr; }
+		const T &operator*() const { return *ptr; }
+	private:
+		T *ptr;
+	};
 private:
 	T *data;
-	std::size_t n;
+	std::size_t dim;
 public:
-	Vector()				: n(0),		data(nullptr) {}
-	Vector(std::size_t n)	: n(n),		data(new T[n]) {}
-	Vector(const Vector& v) : n(v.n),	data(new T[v.n]) {}
-	~Vector() { delete[] data; }
+	Vector()                    : dim(0),     data(nullptr) {}
+	Vector(std::size_t dim)	    : dim(dim),   data(new T[dim]) {}
+	Vector(const Vector& v)     : dim(v.dim), data(new T[v.dim]) {}
+	Vector(Vector&& v) noexcept : dim(v.dim), data(v.data) { v.data = nullptr; }
 
-	std::size_t size() const { return n; }
+	std::size_t size() const { return dim; }
 
-	T get(std::size_t index) const {
+	T& operator[](std::size_t index) const {
+		assert(index < dim);
 		return data[index];
 	}
 
-	void set(std::size_t index, T value) {
-		data[index] = value;
-	}
-	
-	void operator=(const Vector &w) {
-		if (w.n != n) {
-			if (n != 0)
+	Vector &operator=(const Vector &w) {
+		if (w.dim != dim) {
+			if (dim != 0)
 				delete[] data;
-			data = new T[w.n];
-			n = w.n;
+			if (w.dim != 0)
+				data = new T[w.dim];
+			else
+				data = nullptr;
+			dim = w.dim;
 		}
-		for (std::size_t i = 0; i < n; ++i)
+		for (std::size_t i = 0; i < dim; ++i)
 			data[i] = w.data[i];
+		return *this;
+	}
+
+	Vector &operator=(Vector &&w) noexcept {
+		dim = w.dim;
+		std::swap(data, w.data);
+		return *this;
+	}
+
+	iterator begin() { return iterator(data); }
+
+	iterator end() { return iterator(data + dim); }
+
+	~Vector() {
+		if (data != nullptr)
+			delete[] data;
 	}
 };
+
 #endif
